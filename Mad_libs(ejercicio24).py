3ca401624decbd6fbdff9eb0890726f191ea969b
Hotcer <davisguarico27@gmail.com>
2023-01-08 12:36:36 -0500

    Jueguito de completar oraciones llamado 'historias locas', que consiste en completar oraciones

---
+++ Mad_libs(ejercicio24).py
@@ -0,0 +1,8 @@
+adj = input("Programar es tan >> _____ ")
+verbo1 = input('Porque me encanta >> ____ ')
+verbo2 = input('Aprende a programar com >> ____')
+sustantivo_plural = input('y alcanza tus >> _____')
+
+mad_libs = f'Programar es tan {adj} ! Siempre me emociona porque me encanta {verbo1} problemas. Aprende a programar ' \
+           f'con {verbo2} y alcanza tus {sustantivo_plural}!'
+print(mad_libs)
