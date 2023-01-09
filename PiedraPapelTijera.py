9c6dbe11e3e4478154e788c2e1d544cc7f307b83
Hotcer <davisguarico27@gmail.com>
2023-01-09 17:23:37 -0500

    Jueguito de piedra papel y tijera con la computadora ejecutando random.choice

---
+++ PiedraPapelTijera.py
@@ -0,0 +1,23 @@
+import random
+
+
+def juega():
+    usuario = input("Juguemos piedra, papel y tijera: Escribe 'pi' si tu opcion es piedra, si es papel escribe 'pa' "
+                    "si es tijera escribe 'ti'\n").lower()
+    computadora = random.choice(['pi', 'pa', 'ti'])
+    if usuario == computadora:
+        return 'Empate'
+    if gano_el_jugador(computadora, usuario):
+        return 'Ganastes'
+    return 'Perdistes'
+
+
+def gano_el_jugador(oponente, jugador):
+    if ((oponente == 'pi' and jugador == 'ti') or (oponente == 'pa' and jugador == 'pi')
+            or (oponente == 'ti' and jugador == 'pa')):
+        return True
+    else:
+        return False
+
+
+print(juega())
